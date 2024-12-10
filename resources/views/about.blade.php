0d827ec60b8adf01f65a296b62ba4471e53c833c
SirL3X0N <seth@bytech.ca>
2024-12-10 14:16:20 +0100

    boiler plate code

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,10 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline text-green-700">
+
+@extends("layouts.layout")
+
+@section("title","welcome")
+
+@section("content")
+  <h2 class="text-3xl font-bold underline text-green-700">
     Hello world!
-  </h1>
-</body>
-</html>
+  </h2>
+@endsection
