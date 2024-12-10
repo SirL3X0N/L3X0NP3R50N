63cbb460404b9ef120739b4aa5caa1e8a4835764
SirL3X0N <seth@bytech.ca>
2024-12-10 15:52:05 +0100

    finished styling header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,18 +8,8 @@
 </head>
 <body>
     <header>
-        <!-- <h1>Seth Lee</h1>
-        <nav id="bignav">
-            <ul class="nav__links">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Something</a></li>
-                <li><a class="callink" href="#calendar">Calendar</a></li>
-            </ul>
-        </nav>
-        <a class="cta" id="openmodel1"><button>Contact</button></a> -->
-
 <nav class="bg-gray-900 border-gray-200">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 px-4 md:px-0">
 
   <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -28,26 +18,24 @@
         </svg>
     </button>
 
-    <h1 class="block text-white font-serif underline text-2xl tracking-tighter font-extrabold">Seth Lee</h1>
+    <h1 class="block text-white font-serif underline text-2xl tracking-tighter font-extrabold md:mx-2 mx-auto">Seth Lee</h1>
 
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+    <div class="font-mono flex md:ml-auto mx-1">
+    <div class="hidden w-full md:block md:w-auto mr-10 text-center my-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+          <a href="#" class="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0  md:p-0 hover:bg-gray-700 hover:text-red-500 md:hover:bg-transparent">About</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          <a href="#" class="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0  md:p-0 hover:bg-gray-700 hover:text-red-500 md:hover:bg-transparent">Resume</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <a href="#" class="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0  md:p-0 hover:bg-gray-700 hover:text-red-500 md:hover:bg-transparent">Calendar</a>
         </li>
       </ul>
+    </div>
+
+    <button type="button" class="ml-4 text-white bg-red-400 hover:bg-red-600 font-medium rounded-2xl px-4 py-2 text-center text-sm">Contact</button>
     </div>
   </div>
 </nav>
