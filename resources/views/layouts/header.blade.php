487b1b6bcce8f9779a610f72ab2d52733605b656
SirL3X0N <seth@bytech.ca>
2024-12-10 16:45:32 +0100

    Got Hamburger working

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
 <body>
     <header>
 <nav class="bg-gray-900 border-gray-200">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 px-4 md:px-0">
+  <div class="flex flex-wrap items-center justify-between mx-auto py-4 px-4 md:px-0">
 
   <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -18,10 +18,11 @@
         </svg>
     </button>
 
-    <h1 class="block text-white font-serif underline text-2xl tracking-tighter font-extrabold md:mx-2 mx-auto">Seth Lee</h1>
+    <h1 class="block text-white font-serif underline text-2xl tracking-tighter font-extrabold mx-5 mx-auto">Seth Lee</h1>
+    <!-- <div class="font-mono flex md:ml-auto "> -->
+    <button type="button" class="md:hidden p-2 text-white bg-red-500 hover:bg-red-600 font-medium rounded-2xl px-4 py-2 text-center text-sm">Contact</button>
 
-    <div class="font-mono flex md:ml-auto mx-1">
-    <div class="hidden w-full md:block md:w-auto mr-10 text-center my-auto" id="navbar-default">
+    <div class="hidden w-full md:block md:w-auto md:mr-10 text-center my-auto ml-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
           <a href="#" class="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0  md:p-0 hover:bg-gray-700 hover:text-red-500 md:hover:bg-transparent">About</a>
@@ -34,11 +35,21 @@
         </li>
       </ul>
     </div>
+    <button type="button" class="hidden md:block ml-8 mr-5 p-2 text-white bg-red-500 hover:bg-red-600 font-medium rounded-2xl px-4 py-2 text-center text-sm">Contact</button>
 
-    <button type="button" class="ml-4 text-white bg-red-400 hover:bg-red-600 font-medium rounded-2xl px-4 py-2 text-center text-sm">Contact</button>
     </div>
   </div>
 </nav>
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector("[data-collapse-toggle]");
+    const navbar = document.getElementById("navbar-default");
+
+    toggleButton.addEventListener("click", () => {
+      navbar.classList.toggle("hidden"); // Add or remove the `hidden` class
+    });
+  });
+</script>
 
     </header>
     <main>
